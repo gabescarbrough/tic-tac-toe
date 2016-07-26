92a99a0e11d11a6c4e58e4fb6a8e5322f6f94405
gabescarbrough <gabescarbrough@gmail.com>
2016-07-26 16:50:19 -0400

    Work on authorization modals
    Got authorization modals to launch from nav bar. Still need to write scripts for
    forms to communicate with API.

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -2,6 +2,13 @@
 
 const getFormFields = require('../../../lib/get-form-fields');
 
+const showSignUpModal = function showPasswordModal(){
+  $('#sign-up-modal').modal('show');
+};
+
+const showSignInModal = function showPasswordModal(){
+  $('#sign-in-modal').modal('show');
+};
 
 const showChangePasswordModal = function showPasswordModal(){
   $('#change-password-modal').modal('show');
@@ -10,6 +17,8 @@ const showChangePasswordModal = function showPasswordModal(){
 
 
 const addHandlers = () => {
+  $('#sign-up-modal-link').on('click', showSignUpModal);
+  $('#sign-in-modal-link').on('click', showSignInModal);
   $('#change-password-modal-link').on('click', showChangePasswordModal);
 };
 
